3aa9554163f14bab7b1eb05e9706c65150ed4e2d
FrancesDiaz <115190000+francesvdiaz@users.noreply.github.com>
2023-01-27 15:59:02 -0800

    First commit. Html lay out, refined form ids and js variables.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+
+let question1Input = document.getElementById(question1Input).value ;
+console.log(question1Input);
+
+let question2Input = document.getElementById(question2Input).value ;
+console.log(question2Input);
+
+let question3Input = document.getElementById(question3Input).value ;
+console.log(question3Input);
+
+let question4Input = document.getElementById(question4Input).value ;
+console.log(question4Input);
+
+let question5Input = document.getElementById(question5Input)
