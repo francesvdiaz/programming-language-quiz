6372b90b00012c8492d603acf662ad41aeae8463
FrancesDiaz <115190000+francesvdiaz@users.noreply.github.com>
2023-01-31 17:46:29 -0800

    edit function syntax, parameters in answers and values

--- js/scripts.js
+++ js/scripts.js
@@ -1,70 +1,64 @@
 
-form = document.querySelector('form');
+form = document.querySelector('form')
+submitButton = document.getElementById("button")
 
-function getValue() {
-let color = document.getElementById('color').value;
+let color = document.getElementById('color').value
 
-let aliens = document.getElementById('aliens').value;
+let aliens = document.getElementById('aliens').value
 
-let life = document.getElementById('life').value;
+let life = document.getElementById('life').value
 
-let taste = document.getElementById('taste').value;
+let taste = document.getElementById('taste').value
 
-let fun = document.getElementById('fun').value;
+let fun = document.getElementById('fun').value
 
-let info = document.querySelectorAll("input");
-}
+let info = document.querySelectorAll("input")
 
 function decision(e)  {
   e.preventDefault();
    
-  let tally = 0
+  let tally = 0;
 
-if (color === Blue.value) {
+if (color.value === "Blue") {
  tally =+ 1;
-} else if (color === Green) {
+} else if (color === "Green") {
  tally =+ 4
 } 
-if (aliens.value === true) {
-tally =+3;
-} else (aliens.value === false); {
-tally =+ 2;
+if (aliens.value === "true") {
+ tally =+3;
+} else (aliens.value === "false"); {
+ tally =+ 2;
 }
 
-if (life.value === true) {
-tally =+ 3;
-} else (life.value = false) {
-tally =+ 2;
+if (life.value === "True") {
+ tally =+ 3;
+} else (life.value = "False") {
+ tally =+ 2;
 }
-if (taste.value === savory) {
-taly =+ 1;
-} else if (aliens.value === sweet) {
-tally =+ 3
+
+if (taste.value === "savory") {
+ tally =+ 1;
+} else if (aliens.value === "sweet") {
+ tally =+ 3
 } 
 
-if (fun.value === true) {
-tally =+ 3;
-} else if (fun.value === false) {
-tally =+ 1
+if (fun.value === "true") {
+ tally =+ 3;
+} 
+
+ document.getElementById("go").removeAttribute("class","hidden");
+ document.getElementById("python").setAttribute("class","hidden");
+ document.getElementById("react").setAttribute("class","hidden");
 }
-return tally;
+
+if (tally > 9) {
+ document.getElementById("react").removeAttribute("class","hidden");
+ document.getElementById("python").setAttribute("class","hidden");
+ document.getElementById("go").setAttribute("class","hidden");
 }
 
-function displayAnswer()  {
-  document.onsubmit.preventDefault;
-  
-if tally <9
-then document.getElementById('go').removeAttribute('hidden');
-document.getElementById('python').setAttribute('hidden');
-document.getElementById('react).setAttribute('hidden');
-
-if tally > 9
-then document.getElementById('react').removeAttribute('hidden');
-document.getElementById('python').setAttribute('hidden');
-document.getElementById('go').setAttribute('hidden');
-
-if tally < 12;
-then document.getElementById('python').removeAttribute('hidden');
-document.getElementById('go').setAttribute('hidden');
-document.getElementById('react').setAttribute('hidden');
+if (tally < 12) {
+ document.getElementById("python").removeAttribute("class","hidden");
+ document.getElementById("go").setAttribute("class","hidden");
+ document.getElementById("react").setAttribute("class","hidden");
 }
