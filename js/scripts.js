ae02f0ae24f18bc90c592a810fee93fc18cb0c8d
FrancesDiaz <115190000+francesvdiaz@users.noreply.github.com>
2023-02-01 13:38:46 -0800

    Change questions back to drop down menus, remove tally variable from js, using && instead

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,29 @@
-
-const form = document.querySelector("form");
-const submitButton = document.getElementById("button");
-
-let info = document.querySelectorAll("input");
-
-function decision(e)  {
-  e.preventDefault();
+window.onload = function (event) {
+  event.preventDefault 
+  let button = document.getElementById("button");
+  let input = document.querySelectorAll("input");
    
-  let tally = 0;
+  button.addEventListener("click", function() {
+
   let color = getElementById("color").value;
   let aliens = getElementById("aliens").value;
   let life = getElementById("life").value;
   let taste = getElementById("taste").value;
   let fun = getElementById("fun").value;
 
-  if (color === "Blue") {
-   tally =+ 1;
-  } else if (color === "Red") {
-   tally =+ 4
+  if (color === "Blue" && aliens === "True" && life === "True" && taste === "Sweet" && fun === "True") {
+    document.getElementById("go").removeAttribute("class","hidden");
+    document.getElementById("python").setAttribute("class","hidden");
+    document.getElementById("react").setAttribute("class","hidden");
+  } else if (color === "Red" && aliens === "False" && life === "False" && taste === "Savory" && fun === "False") {
+    document.getElementById("react").removeAttribute("class","hidden");
+    document.getElementById("python").setAttribute("class","hidden");
+    document.getElementById("go").setAttribute("class","hidden");
   } 
-  if (aliens === "True") {
-   tally =+3;
-  } else (aliens.value === "False"); {
-   tally =+ 2;
-  }
-
-  if (life. === "True") {
-   tally =+ 3;
-  } else (life === "False") {
-   tally =+ 2;
-  }
-
-  if (taste === "Savory") {
-   tally =+ 1;
-  } else if (aliens.value === "Sweet") {
-   tally =+ 3
-  } 
-
-  if (fun === "True") {
-   tally =+ 3;
-  } 
-
-  if (tally < 9) {
-   document.getElementById("go").removeAttribute("class","hidden");
-   document.getElementById("python").setAttribute("class","hidden");
-  document.getElementById("react").setAttribute("class","hidden");
-  } else if (tally > 9 && tally < 12)  {
-   document.getElementById("react").removeAttribute("class","hidden");
-   document.getElementById("python").setAttribute("class","hidden");
-   document.getElementById("go").setAttribute("class","hidden");
-  } else  {
-   document.getElementById("python").removeAttribute("class","hidden");
-   document.getElementById("go").setAttribute("class","hidden");
-  document.getElementById("react").setAttribute("class","hidden");
-  }
-info.forEach((input) => (info.value = ""));
-}
-
+  else (aliens === "True") {
+    document.getElementById("python").removeAttribute("class","hidden");
+    document.getElementById("go").setAttribute("class","hidden");
+    document.getElementById("react").setAttribute("class","hidden");
+  };
+ });
+ }
