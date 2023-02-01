74e4576bcf6d8373fed2698b7a1eb6cc1c5b8d44
FrancesDiaz <115190000+francesvdiaz@users.noreply.github.com>
2023-01-31 19:46:07 -0800

    New commit to
    qq
    qq

--- js/scripts.js
+++ js/scripts.js
@@ -1,64 +1,59 @@
 
-form = document.querySelector('form')
-submitButton = document.getElementById("button")
+const form = document.querySelector("form");
+const submitButton = document.getElementById("button");
 
-let color = document.getElementById('color').value
-
-let aliens = document.getElementById('aliens').value
-
-let life = document.getElementById('life').value
-
-let taste = document.getElementById('taste').value
-
-let fun = document.getElementById('fun').value
-
-let info = document.querySelectorAll("input")
+let info = document.querySelectorAll("input");
 
 function decision(e)  {
   e.preventDefault();
    
   let tally = 0;
+  let color = getElementById("color").value;
+  let aliens = getElementById("aliens").value;
+  let life = getElementById("life").value;
+  let taste = getElementById("taste").value;
+  let fun = getElementById("fun").value;
 
-if (color.value === "Blue") {
- tally =+ 1;
-} else if (color === "Green") {
- tally =+ 4
-} 
-if (aliens.value === "true") {
- tally =+3;
-} else (aliens.value === "false"); {
- tally =+ 2;
+  if (color === "Blue") {
+   tally =+ 1;
+  } else if (color === "Red") {
+   tally =+ 4
+  } 
+  if (aliens === "True") {
+   tally =+3;
+  } else (aliens.value === "False"); {
+   tally =+ 2;
+  }
+
+  if (life. === "True") {
+   tally =+ 3;
+  } else (life === "False") {
+   tally =+ 2;
+  }
+
+  if (taste === "Savory") {
+   tally =+ 1;
+  } else if (aliens.value === "Sweet") {
+   tally =+ 3
+  } 
+
+  if (fun === "True") {
+   tally =+ 3;
+  } 
+
+  if (tally < 9) {
+   document.getElementById("go").removeAttribute("class","hidden");
+   document.getElementById("python").setAttribute("class","hidden");
+  document.getElementById("react").setAttribute("class","hidden");
+  } else if (tally > 9 && tally < 12)  {
+   document.getElementById("react").removeAttribute("class","hidden");
+   document.getElementById("python").setAttribute("class","hidden");
+   document.getElementById("go").setAttribute("class","hidden");
+  } else  {
+   document.getElementById("python").removeAttribute("class","hidden");
+   document.getElementById("go").setAttribute("class","hidden");
+  document.getElementById("react").setAttribute("class","hidden");
+  }
+info.forEach((input) => (info.value = ""));
 }
 
-if (life.value === "True") {
- tally =+ 3;
-} else (life.value = "False") {
- tally =+ 2;
-}
-
-if (taste.value === "savory") {
- tally =+ 1;
-} else if (aliens.value === "sweet") {
- tally =+ 3
-} 
-
-if (fun.value === "true") {
- tally =+ 3;
-} 
-
- document.getElementById("go").removeAttribute("class","hidden");
- document.getElementById("python").setAttribute("class","hidden");
- document.getElementById("react").setAttribute("class","hidden");
-}
-
-if (tally > 9) {
- document.getElementById("react").removeAttribute("class","hidden");
- document.getElementById("python").setAttribute("class","hidden");
- document.getElementById("go").setAttribute("class","hidden");
-}
-
-if (tally < 12) {
- document.getElementById("python").removeAttribute("class","hidden");
- document.getElementById("go").setAttribute("class","hidden");
- document.getElementById("react").setAttribute("class","hidden");
-}
