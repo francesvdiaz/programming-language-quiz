53791d477265498d412120bf1def0e5a68d9933c
FrancesDiaz <115190000+francesvdiaz@users.noreply.github.com>
2023-02-03 14:39:02 -0800

    rework js, logic and branching statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,27 @@
-window.onload = function (event) {
-  event.preventDefault 
-  let button = document.getElementById("button");
-  let input = document.querySelectorAll("input");
-   
-  button.addEventListener("click", function() {
+window.addEventListener("load", function() { 
+  const button = document.getElementById("button");
 
-  let color = getElementById("color").value;
-  let aliens = getElementById("aliens").value;
-  let life = getElementById("life").value;
-  let taste = getElementById("taste").value;
-  let fun = getElementById("fun").value;
+  button.addEventListener("click", function(event) {
+    event.preventDefault();
 
-  if (color === "Blue" && aliens === "True" && life === "True" && taste === "Sweet" && fun === "True") {
-    document.getElementById("go").removeAttribute("class","hidden");
-    document.getElementById("python").setAttribute("class","hidden");
-    document.getElementById("react").setAttribute("class","hidden");
-  } else if (color === "Red" && aliens === "False" && life === "False" && taste === "Savory" && fun === "False") {
-    document.getElementById("react").removeAttribute("class","hidden");
-    document.getElementById("python").setAttribute("class","hidden");
-    document.getElementById("go").setAttribute("class","hidden");
-  } 
-  else (aliens === "True") {
-    document.getElementById("python").removeAttribute("class","hidden");
-    document.getElementById("go").setAttribute("class","hidden");
-    document.getElementById("react").setAttribute("class","hidden");
-  };
- });
- }
+    const color = document.getElementById("color").value;
+    const aliens = document.getElementById("aliens").value;
+    const car = document.getElementById("car").value;
+    const taste = document.getElementById("taste").value;
+    const fun = document.getElementById("fun").value;
+
+    if (color === "blue" && aliens === "yes" && car === "yes" && taste === "savory" && fun === "yes") {
+      document.getElementById("go").removeAttribute("class","hidden");
+      document.getElementById("python").setAttribute("class","hidden");
+      document.getElementById("react").setAttribute("class","hidden");
+    } else if (color == "blue" && aliens == "yes" && car == "yes" && taste == "sweet" && fun == "yes") {
+      document.getElementById("react").removeAttribute("class","hidden");
+      document.getElementById("python").setAttribute("class","hidden");
+      document.getElementById("go").setAttribute("class","hidden");
+    } else {
+      document.getElementById("python").removeAttribute("class","hidden");
+      document.getElementById("go").setAttribute("class","hidden");
+      document.getElementById("react").setAttribute("class","hidden");
+    }
+});
+});
